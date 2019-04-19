fc251a1573254f3fff9b9d38cc58ab93371f5a49
FannieXue <haifang77@126.com>
2019-04-19 19:12:30 +0800

    about zipkin

---
+++ src/main/java/com/mars/person/user/User.java
@@ -0,0 +1,14 @@
+package com.mars.person.user;
+
+
+/**
+ * @project person
+ * @author: FannieXue
+ * @time 2019-04-18
+ */
+public class User {
+
+    Long id;
+
+    String name;
+}
--- src/main/java/com/mars/person/user/UserController.java
+++ src/main/java/com/mars/person/user/UserController.java
@@ -1,12 +1,16 @@
 package com.mars.person.user;
 
 import com.mars.depot.person.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @project person
@@ -17,11 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/person/v1/users")
 public class UserController {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
-    public String getUser(@PathVariable("userId") Long userId) {
+    public String getUser(@PathVariable("userId") Long userId,
+                          HttpServletRequest request) {
+        logger.info("person.UserController --> getUser(), TraceId={}, SpanId={} ",
+                request.getHeader("X-B3-TraceId"),
+                request.getHeader("X-B3-SpanId"));
+
         String services = "Services: " + discoveryClient.getServices();
 //        Json
         User u = new User();
